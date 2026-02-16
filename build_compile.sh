f3c866e99493f2009d19c6ac70f140d20db5a187
Guyen Gn <guyen900@gmail.com>
2026-02-16 14:23:16 -0800

    Update build_compile.sh - corrected logic.

--- build_compile.sh
+++ build_compile.sh
@@ -5,7 +5,7 @@ ret=0
 for i in ./*.cpp ; do
     echo '--------- building and running $i ... ---------'
     g++ -w $i -o out/$i.out && ./out/$i.out
-    [[ $? -ne 0 ]] && ret=1
+    [[ $? -ne 0 ]] || ret=1
 done
 popd
 exit $ret
